628598512e86cdaca377a1b0c3688bcd403e61af
kthong3 <kthongsakounh@gmail.com>
2018-03-21 23:27:38 -0500

    add user and admin routes, view for new session, and update users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
-  def create
-    @user = User.new(user_params)
-  end
+  # def create
+  #   @user = User.new(user_params)
+  # end
 
   def show
     @user = User.find(session[:user_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
   resources :cinemas, only: [:index, :show]
 
-  resources :films do
-    resources :screenings
+  resources :films, only: [:index, :show] do
+    resources :screenings, only: [:index, :show]
+  end
+
+  namespace :admin do
+    resources :films, except: [:index, :show] do
+         resources :screenings, except: [:index, :show]
+    end
+    resources :cinemas, except: [:index, :show]
   end
 
   root 'films#index'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+  # get 'signup' => 'users#new'
 end
